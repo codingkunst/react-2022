40f274b1eaf61826c0d30a2909ffc3156c23c6a6
codingkunst <ryoo6931@gmail.com>
2024-07-10 15:08:11 +0900

    CREATE 기능 추가

--- src/App.js
+++ src/App.js
@@ -56,23 +56,43 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder="제목" /></p>
+        <p><textarea name="body" placeholder="내용"></textarea></p>
+        <p><input type="submit" value="Create" /></p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   // const _mode = useState('WELCOME');
   // const mode = _mode[0];
   // const setMode = _mode[1];
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: "HTML", body: "html is ..." },
     { id: 2, title: "CSS", body: "CSS is ..." },
     { id: 3, title: "JS", body: "JS is ..." },
-  ];
+  ]);
 
   let content = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
   } else if (mode === "READ") {
-    let title, body = null;
+    let title,
+      body = null;
     for (let i = 0; i < topics.length; i++) {
       if (topics[i].id === id) {
         title = topics[i].title;
@@ -80,6 +100,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === "CREATE") {
+    content = <Create onCreate={(title, body) => {
+      const newTopic = {id:nextId, title:title, body:body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>;
   }
 
   return (
@@ -101,6 +131,16 @@ function App() {
 
       {/* <Article title='Welcome' body='Hello, WEB'></Article> */}
       {content}
+
+      <a
+        href="/create"
+        onClick={(event) => {
+          event.preventDefault();
+          setMode("CREATE");
+        }}
+      >
+        Create
+      </a>
     </div>
   );
 }
